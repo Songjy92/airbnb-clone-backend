9ae678688bc74ff3185387ec45b25a94cf6f22b2
Jaeyeol Song <songjy92@gmail.com>
2023-04-03 23:10:06 +0900

    20230403-user app created

--- houses/admin.py
+++ houses/admin.py
@@ -11,4 +11,3 @@ from .models import House
 class HouseAdmin(admin.ModelAdmin):
     list_display = ("name", "price_per_night", "address", "pet_allowed")
     list_filter = ("price_per_night", "pet_allowed")
-    search_fields = ("address",)
