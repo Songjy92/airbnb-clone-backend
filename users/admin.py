df2dc3234bd796db4e8a7dad00e74f11559c5b24
Song Jaeyeol <songjy92@gmail.com>
2023-04-08 14:16:45 +0900

    section3 models

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,40 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
+
 # Register your models here.
 
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
-    pass
+    fieldsets = (
+        (
+            "profile",
+            {
+                "fields": (
+                    "username",
+                    "name",
+                    "email",
+                    "is_host",
+                ),
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "avatar",
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                    "gender",
+                    "language",
+                    "currency",
+                ),
+                "classes": ("collapse",),
+            },
+        ),
+    )
+    list_display = ("username", "name", "email")
--- users/models.py
+++ users/models.py
@@ -1,12 +1,51 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+
 # Create your models here.
 
 
 class User(AbstractUser):
+    class GenderChoices(models.TextChoices):
+        # tuple 앞의 값은 DB에 저장되는 값, 뒤에는 admin panel 에서 보여지는 값
+        MALE = ("male", "Male")
+        FEMALE = ("female", "Female")
+
+    class LanguageChoices(models.TextChoices):
+        KR = ("kr", "Korean")
+        EN = ("en", "English")
+
+    class CurrencyChoices(models.TextChoices):
+        WON = "won", "Korean Won"
+        USD = "usd", "Dollar"
+
     first_name = models.CharField(
-        _("first name"), max_length=150, editable=False)
+        ("first name"),
+        max_length=150,
+        editable=False,
+    )
     last_name = models.CharField(
-        _("last name"), max_length=150, editable=False)
-    name = models.CharField(max_length=150)
-    is_host = models.BooleanField(default=False)
+        ("last name"),
+        max_length=150,
+        editable=False,
+    )
+    name = models.CharField(
+        max_length=150,
+        default="",
+    )
+    is_host = models.BooleanField(
+        default=False,
+    )
+    avatar = models.ImageField(blank=True)
+
+    gender = models.CharField(
+        max_length=10,
+        choices=GenderChoices.choices,
+    )
+    language = models.CharField(
+        max_length=3,
+        choices=LanguageChoices.choices,
+    )
+    currency = models.CharField(
+        max_length=5,
+        choices=CurrencyChoices.choices,
+    )
